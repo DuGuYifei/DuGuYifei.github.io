3aed2dee8009061a8df215ee459d51421cdbaa4d
DuGuYifei <liuyifei97@163.com>
2025-04-20 13:55:54 +0200

    update head of icons

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -8,4 +8,9 @@ export const head: HeadConfig[] = [
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+  ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+  ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+  ['link', { rel: 'manifest', href: '/site.webmanifest' }],
 ]
